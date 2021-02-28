bad7f0e536f663aad473073f0f67c67114dbe9d4
Dainius Karalevicius <dainius.karalevicus@gmail.com>
2021-02-28 11:25:08 +0200

    Gitignore check

--- src/main/java/sda/weatherlady/Main.java
+++ src/main/java/sda/weatherlady/Main.java
@@ -3,6 +3,7 @@ package sda.weatherlady;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Mano projektas");
+        System.out.println("Gitignore check");
 
     }
 }
