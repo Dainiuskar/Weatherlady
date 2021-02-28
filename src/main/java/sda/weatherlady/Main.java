d12c29b901efa4ba9adc691f7ca86dbdd35a98b9
Dainius Karalevicius <dainius.karalevicus@gmail.com>
2021-02-28 11:59:38 +0200

    Hello

--- src/main/java/sda/weatherlady/Main.java
+++ src/main/java/sda/weatherlady/Main.java
@@ -3,7 +3,5 @@ package sda.weatherlady;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Check");
-        System.out.println("Check");
     }
 }
